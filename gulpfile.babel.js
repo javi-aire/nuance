3e0ba09ec36b3089d16fdd339450b47653f07369
Javier Williams <javier.williams720@gmail.com>
2017-05-18 15:46:36 -0700

    Initial commit, copied over files/folders to repo folder

---
+++ gulpfile.babel.js
@@ -0,0 +1,98 @@
+/**
+* Import necessary plugins
+*/
+import gulp from 'gulp';
+import browser from 'browser-sync';
+import htmlMin from 'gulp-htmlmin';
+import cssMin from 'gulp-clean-css';
+import imgMin from 'gulp-imagemin';
+import jsMin from 'gulp-uglify';
+import seq from 'run-sequence';
+import babel from 'gulp-babel';
+
+/**
+* Object map of paths for use with tasks
+*/
+const paths = {
+  src: '/',
+  js: './js/**/*.js',
+  images: './client/assets/images/**/*',
+  html: './*.html',
+  css: './css/**/*.css',
+  dest: 'dist'
+};
+
+/**
+* compile/minify css files
+*/
+gulp.task("css", () => {
+  return gulp.src(paths.css)
+      .pipe(cssMin())
+      .pipe(gulp.dest(`${paths.dest}/css`))
+      .pipe(browser.reload({
+        stream: true
+      }));
+});
+
+/**
+* minify html but compiles css first
+*/
+gulp.task('html', ['css'], () => {
+  return gulp.src(paths.html)
+    .pipe(htmlMin({collapseWhitespace: true}))
+    .pipe(gulp.dest(`${paths.dest}/`));
+});
+
+/**
+* compile and minify JS (even though there isn't any)
+*/
+gulp.task('babel', () => {
+  return gulp.src(paths.js)
+    .pipe(babel())
+    .pipe(jsMin())
+    .pipe(gulp.dest(`${paths.dest}/js`));
+});
+
+
+/**
+* watch js, html, scss files for changes
+* compiles/minifies all on any change
+*/
+gulp.task('watch', ['serve'], () => {
+  gulp.watch(`${paths.src}/**/*.{js,scss,html}`, ['css', 'html', 'babel', browser.reload]);
+});
+
+
+/**
+* serve on port 3175 from base directory dist/
+* also exposes node_modules to dist so nothing will break :)
+*/
+gulp.task('serve', () => {
+  browser({
+    port: process.env.PORT || 3000,
+    ghostMode: false,
+    open: false,
+    server: {
+      baseDir: `${paths.dest}/`,
+      routes : {
+        './node_modules': './node_modules'
+      }
+    }
+  });
+});
+
+
+/*
+* uses run-sequence to run every task in order
+*/
+gulp.task('build', () => {
+  return seq('css', 'html', 'babel', 'watch');
+});
+
+
+/*
+* default task -- calls the build command
+*/
+gulp.task('default', (done) => {
+  seq('build', done);
+});
